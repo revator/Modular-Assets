785a473260794a52b77662d1815f2d1e2f32d7b0
Unknown <sven.ulrich87@googlemail.com>
2018-05-12 02:08:32 +0100

    Added modular assets
    Ladder_Tunnel, Ladder_Tunnel_Ending, Wall_Tunnel.

---
+++ Modular Asset Creation/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    CharacterController charControl;
+    public float walkSpeed;
+    public float sprintSpeed;
+
+    void Awake()
+    {
+        charControl = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MovePlayer();
+	}
+
+    void MovePlayer()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float verti = Input.GetAxis("Vertical");
+
+        float maxSpeed = walkSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            maxSpeed += walkSpeed;
+        }
+        Vector3 moveDirSide = transform.right * horiz * maxSpeed;
+        Vector3 moveDirForwards = transform.forward * verti * maxSpeed;
+        Debug.Log(maxSpeed);
+
+
+        charControl.SimpleMove(moveDirSide);
+        charControl.SimpleMove(moveDirForwards);
+    }
+}
